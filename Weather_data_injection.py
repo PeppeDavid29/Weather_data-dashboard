ddb4a6ee8ac3a0f51ed14184d33978f2a5e18c2f
PeppeDavid29 <Sharonfrancissallu@gmail.com>
2024-08-09 13:00:57 +0530

    commit changes

--- Weather_data_injection.py
+++ Weather_data_injection.py
@@ -1,21 +1,18 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+from Weather_data_injection import inject_data  # Ensure this is correct
 
 spark = SparkSession.builder \
     .appName("WeatherDataProcessing") \
     .getOrCreate()
 
 try:
-   
     df_spark = spark.read.csv(r"C:/Users/sharo/weather_data.csv", header=True, inferSchema=True)
     df_spark.show()
 
-
     ordered_df = df_spark.orderBy("location")
 
-
     output_csv_path = "D:/sql/ordered_by_city.csv"
-
 
     ordered_df.coalesce(1).write.csv(output_csv_path, header=True, mode="overwrite")
 
