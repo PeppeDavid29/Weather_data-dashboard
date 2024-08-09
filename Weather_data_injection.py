a889356767741509d09155ca0686b2ee50c4e67e
PeppeDavid29 <Sharonfrancissallu@gmail.com>
2024-08-09 23:32:47 +0530

    commit changes

---
+++ Weather_data_injection.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder \
+    .appName("WeatherDataProcessing") \
+    .getOrCreate()
+
+try:
+   
+    df_spark = spark.read.csv(r"C:/Users/sharo/weather_data.csv", header=True, inferSchema=True)
+    df_spark.show()
+
+
+    ordered_df = df_spark.orderBy("location")
+
+
+    output_csv_path = "D:/sql/ordered_by_city.csv"
+
+
+    ordered_df.coalesce(1).write.csv(output_csv_path, header=True, mode="overwrite")
+
+    print(f"Weather data ordered by city successfully saved to {output_csv_path}")
+
+except Exception as e:
+    print(f"Unexpected error: {e}")
+
+finally:
+    spark.stop()
+
